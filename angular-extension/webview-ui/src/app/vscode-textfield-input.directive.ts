833ed5daa98c9eb0aca1bab72af97b30e683a61b
Fauth Dirk (ETAS/ENA) <Dirk.Fauth@etas.com>
2025-03-05 08:04:57 +0000

    Added usage of VSCode Elements with Angular

---
+++ angular-extension/webview-ui/src/app/vscode-textfield-input.directive.ts
@@ -0,0 +1,62 @@
+import {
+  Directive,
+  ElementRef,
+  forwardRef,
+  HostListener,
+  Provider,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const VSCODE_TEXTFIELD_INPUT_VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => VscodeTextfieldInputDirective),
+  multi: true,
+};
+
+@Directive({
+  selector: 'vscode-textfield',
+  providers: [VSCODE_TEXTFIELD_INPUT_VALUE_ACCESSOR],
+})
+export class VscodeTextfieldInputDirective implements ControlValueAccessor {
+  val = '';
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  get value() {
+    return this.val;
+  }
+
+  set value(value: any) {
+    if (!value || value === this.val) return;
+    this.val = value;
+    this.elRef.nativeElement.value = this.value;
+
+    this.onChange(this.val);
+    this.onTouched();
+  }
+
+  constructor(private elRef: ElementRef) {}
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: string): void {
+    this.val = value;
+    this.elRef.nativeElement.value = value;
+  }
+
+  @HostListener('change', ['$event'])
+  onHostChange(event: Event) {
+    this.value = this.elRef.nativeElement.value;
+  }
+
+  @HostListener('input', ['$event'])
+  onHostInput(event: Event) {
+    this.value = this.elRef.nativeElement.value;
+  }
+}
